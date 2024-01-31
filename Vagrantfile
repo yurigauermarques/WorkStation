be2ac823af8ac76abd1c534f2cba1b9a8447c364
Yuri <yuri.marques@al.mt.gov.br>
2024-01-31 16:14:30 -0400

    build: :construction: ADD Docker e Docker-compose

--- Vagrantfile
+++ Vagrantfile
@@ -6,27 +6,27 @@ require 'yaml'
 
 env = YAML.load_file('env.yaml')
 
+
 Vagrant.configure("2") do |config|
-  env.each do |env|
-    config.vm.define env['name'] do |host|
-      host.vm.box = env['box']
-      host.vm.hostname = env['hostname']
-      host.vm.network "private_network", ip: env['ipaddress']
-      host.vm.network "public_network", :bridge => env['bridge']
-      host.vm.synced_folder env['project_dir'], "/projetos"
-      host.vm.provider "virtualbox" do |vb|
-        vb.name = env['name']
-        vb.cpus = env['cpu']
-        vb.memory = env['memory']
-        vb.customize ["modifyvm", :id, "--groups", env['group']]
-      end
-      host.vm.provision "file", source: ".ssh/config", destination: ".ssh/config"
-      host.vm.provision "file", source: "~/.ssh/id_rsa", destination: ".ssh/id_rsa"
-      host.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: ".ssh/id_rsa.pub"
-      #host.vm.provision "shell", path: "init.sh" privileged false
-      host.vm.provision "shell", path: "init.sh", privileged: false
-      host.vm.provision "file", source: "~/.gitconfig", destination: ".gitconfig"
-    end
+
+  config.vm.define env['name']
+
+  config.ssh.forward_agent = true
+
+  config.vm.box = env['box']
+  config.vm.hostname = env['hostname']
+  config.vm.network "private_network", ip: env['ipaddress']
+  
+  config.vm.network "public_network", :bridge => env['bridge']
+  config.vm.synced_folder env['project_dir'], "/projetos"
+  config.vm.provider "virtualbox" do |vb|
+    vb.name = env['name']
+    vb.cpus = env['cpu']
+    vb.memory = env['memory']
+    vb.customize ["modifyvm", :id, "--groups", env['group']]
   end
+  
+  config.vm.provision "shell", path: env['provision']
+  config.vm.provision "file", source: "~/.gitconfig", destination: ".gitconfig"
 end
 
--- init.sh
+++ init.sh
@@ -1,63 +1,25 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install vim -y
-sudo apt-get install curl -y
-sudo apt-get install make -y
-sudo apt-get install net-tools -y
-sudo apt-get install cifs-utils -y
-sudo apt-get install nfs-common -y
 
-sudo apt-get install git -y
+# Update, best practices and install dependencies
+sudo apt-get update -y \
+    && apt-get install -y --no-install-recommends \
+        curl \
+        make \
+        git \
+    && true
 
-eval `ssh-agent -s`
-#ssh-add ~/.ssh/id_rsa
+## Install Docker and Docker-Compose
+cat <<EOF | sudo tee -a /home/vagrant/.bashrc
+export HOST_IP=\$(ip route get \$(ip route show 0.0.0.0/0 | grep -oP 'via \K\S+') | grep -oP 'src \K\S+')
+EOF
 
+curl -fsSL https://get.docker.com -o install-docker.sh
+sudo sh install-docker.sh
+sudo usermod -aG docker ${USER}
 
-ls ~/ -lhas
-sudo ls ~/ -lhas
+COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-#echo \
-#  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-#  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt-get update
-#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-#sudo usermod -aG docker vagrant
-#
-#sudo systemctl enable docker.service
-#sudo systemctl enable containerd.service
-#
-#sudo mkdir -p /usr/local/lib/docker/cli-plugins
-#sudo curl -SL "https://github.com/docker/compose/releases/download/v2.17.2/docker-compose-linux-x86_64" -o /usr/local/lib/docker/cli-plugins/docker-compose
-#sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
-#sudo ln -s /usr/local/lib/docker/cli-plugins/docker-compose /usr/bin/docker-compose
-#
-#cat <<EOF | sudo tee -a /home/vagrant/.bashrc
-#export HOST_IP=\$(ip route get \$(ip route show 0.0.0.0/0 | grep -oP 'via \K\S+') | grep -oP 'src \K\S+')
-#EOF
-#
-#sudo mkdir -p /etc/docker
-#cat <<EOF | sudo tee /etc/docker/daemon.json
-#{
-#  "bip": "172.27.1.1/16",
-#  "default-address-pools": [
-#    {
-#      "base": "172.26.1.1/16",
-#      "size": 24
-#    }
-#  ],
-#  "dns": ["192.168.21.96", "192.168.21.97"],
-#  "dns-search": ["al.mt.gov.br"],
-#  "exec-opts": ["native.cgroupdriver=systemd"],
-#  "log-driver": "json-file",
-#  "log-opts": {
-#    "max-size": "100m"
-#  },
-#  "storage-driver": "overlay2",
-#  "metrics-addr" : "0.0.0.0:9323",
-#  "experimental" : true
-#}
-#EOF
 
 
